41566a33da7e326a6e7f61c5d6faac320ea57bf0
Dmytro Popov <dmytro_popov@epam.com>
2020-10-22 21:47:39 +0300

    Rework getting loan info to fix issues with incorrect batching

--- src/main/java/org/folio/mappers/CirculationToRtacMapper.java
+++ src/main/java/org/folio/mappers/CirculationToRtacMapper.java
@@ -9,6 +9,7 @@ import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 import java.util.TimeZone;
+import java.util.stream.IntStream;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.folio.rest.jaxrs.model.Item;
@@ -24,43 +25,42 @@ public class CirculationToRtacMapper {
   }
 
   /**
-   * RTac mapper.
+   * Enriches instance items with with loan due date(if loan exists for an item).
    *
-   * @param loansObj loan's representation in json
-   * @param items items representation in json
-   * @return list of item domain objects
+   * @param loanList - Loans in batches of #CirculationClient.CIRCULATION_BATCH_SIZE
+   * @param items - instance items
+   * @return - instance items enriched with loan due date(if it exists)
    */
-  public List<Item> mapToRtac(JsonObject loansObj, List<Item> items) {
-    var updatedItems = new ArrayList<Item>();
-    try {
-      var totalRecords = loansObj.getValue("totalRecords");
-      logger.info(
-          "Open loans received from circulation: {}", totalRecords == null ? 0 : totalRecords);
-      final JsonArray loans = loansObj.getJsonArray("loans");
-      for (Item item : items) {
-        final Optional<JsonObject> loan =
-            loans.stream()
-                .map(a -> (JsonObject) a)
-                .filter(a -> a.getString("itemId").equals(item.getId()))
-                .findFirst();
-        loan.ifPresentOrElse(
-            l -> {
-              final String dueDateString = l.getString("dueDate");
-              logger.debug("Loan {} due date: {}", l.getString("id"), dueDateString);
-              Date dueDate = null;
-              try {
-                dueDate = dateFormat.parse(dueDateString);
-              } catch (ParseException e) {
-                updatedItems.add(item);
+  public List<Item> updateItemsWithLoanDueDate(List<JsonObject> loanList, List<Item> items) {
+    var updatedItems = new ArrayList<>(items);
+    IntStream.range(0, items.size())
+        .forEach(
+            i -> {
+              for (var loansObj : loanList) {
+                var totalRecords = loansObj.getValue("totalRecords");
+                logger.info(
+                    "Open loans received from circulation: {}",
+                    totalRecords == null ? 0 : totalRecords);
+                final JsonArray loans = loansObj.getJsonArray("loans");
+                final Optional<JsonObject> loan =
+                    loans.stream()
+                        .map(a -> (JsonObject) a)
+                        .filter(a -> a.getString("itemId").equals(updatedItems.get(i).getId()))
+                        .findFirst();
+                loan.ifPresent(
+                    l -> {
+                      final String dueDateString = l.getString("dueDate");
+                      logger.debug("Loan {} due date: {}", l.getString("id"), dueDateString);
+                      Date dueDate = null;
+                      try {
+                        dueDate = dateFormat.parse(dueDateString);
+                      } catch (ParseException e) {
+                        logger.warn(e.getMessage(), e);
+                      }
+                      updatedItems.set(i, items.get(i).withDueDate(dueDate));
+                    });
               }
-              updatedItems.add(item.withDueDate(dueDate));
-            },
-            () -> updatedItems.add(item));
-      }
-    } catch (Exception e) {
-      logger.error(e.getMessage(), e);
-      return items;
-    }
+            });
     return updatedItems;
   }
 }
